85b7a794e16af1fdd4d4c2a00992250a7bdbe395
unknown <alex.chinchi02@gmail.com>
2025-05-21 15:16:31 +0200

    cambios

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "EMB Portfolio",
+  description: "Nuestros proyectos y acerca de nosotros",
 };
 
 export default function RootLayout({

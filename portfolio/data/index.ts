379890dd8040ac7a77fde6767c9fbda1a96df6a5
unknown <alex.chinchi02@gmail.com>
2025-05-21 11:33:09 +0200

    cambios

---
+++ portfolio/data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "Nosotros", link: "#about" },
+  { name: "Proyectos", link: "#projects" },
+  { name: "Testimonios", link: "#testimonials" },
+  { name: "Contacto", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Priorizamos la colaboración con el cliente, fomentando una comunicación abierta",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Somos flexibles con las comunicaciones en diferentes zonas horarias",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Nuestras tecnologías",
+    description: "Constantemente buscamos mejorar",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiastas de la tecnología con pasión por el desarrollo.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Actualmente desarrollando una biblioteca de animación JS",
+    description: "La información interna",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "¿Quieres iniciar un proyecto juntos?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Sistema Solar 3D para Explorar",
+    des: "Explora las maravillas de nuestro sistema solar con esta cautivadora simulación 3D de los planetas usando Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Aplicación de Videoconferencias",
+    des: "Simplifica tu experiencia de videoconferencia con Yoom. Conéctate sin problemas con colegas y amigos.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Aplicación tipo Canva",
+    des: "Una aplicación REAL de Software como Servicio con funciones de IA y un sistema de pagos y créditos utilizando la última tecnología.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Sitio Web 3D Animado de Apple iPhone",
+    des: "Recreación del sitio web del iPhone 15 Pro de Apple, combinando animaciones GSAP y efectos 3D de Three.js.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Colaborar con el Equipo EMB fue un placer absoluto. Su profesionalismo, prontitud y dedicación para ofrecer resultados excepcionales fueron evidentes durante todo nuestro proyecto. El entusiasmo del Equipo EMB por cada faceta del desarrollo realmente destaca. Si buscas elevar tu sitio web y tu marca, el Equipo EMB es el socio ideal.",
+    name: "Miguel Jiménez",
+    title: "Director de AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Colaborar con el Equipo EMB fue un placer absoluto. Su profesionalismo, prontitud y dedicación para ofrecer resultados excepcionales fueron evidentes durante todo nuestro proyecto. El entusiasmo del Equipo EMB por cada faceta del desarrollo realmente destaca. Si buscas elevar tu sitio web y tu marca, el Equipo EMB es el socio ideal.",
+    name: "Miguel Jiménez",
+    title: "Director de AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Colaborar con el Equipo EMB fue un placer absoluto. Su profesionalismo, prontitud y dedicación para ofrecer resultados excepcionales fueron evidentes durante todo nuestro proyecto. El entusiasmo del Equipo EMB por cada faceta del desarrollo realmente destaca. Si buscas elevar tu sitio web y tu marca, el Equipo EMB es el socio ideal.",
+    name: "Miguel Jiménez",
+    title: "Director de AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Colaborar con el Equipo EMB fue un placer absoluto. Su profesionalismo, prontitud y dedicación para ofrecer resultados excepcionales fueron evidentes durante todo nuestro proyecto. El entusiasmo del Equipo EMB por cada faceta del desarrollo realmente destaca. Si buscas elevar tu sitio web y tu marca, el Equipo EMB es el socio ideal.",
+    name: "Miguel Jiménez",
+    title: "Director de AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Colaborar con el Equipo EMB fue un placer absoluto. Su profesionalismo, prontitud y dedicación para ofrecer resultados excepcionales fueron evidentes durante todo nuestro proyecto. El entusiasmo del Equipo EMB por cada faceta del desarrollo realmente destaca. Si buscas elevar tu sitio web y tu marca, el Equipo EMB es el socio ideal.",
+    name: "Miguel Jiménez",
+    title: "Director de AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Ingeniero Frontend Practicante",
+    desc: "Asistencia en el desarrollo de una plataforma web utilizando React.js, mejorando la interactividad.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Desarrollo de Apps Móviles - EMB Tech",
+    desc: "Diseño y desarrollo de aplicaciones móviles para plataformas iOS y Android utilizando React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Proyecto Freelance de Desarrollo de Apps",
+    desc: "Liderazgo en el desarrollo de una aplicación móvil para un cliente, desde el concepto inicial hasta la implementación en las tiendas de aplicaciones.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Desarrollador Frontend Principal",
+    desc: "Desarrollo y mantenimiento de características orientadas al usuario utilizando tecnologías frontend modernas.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];

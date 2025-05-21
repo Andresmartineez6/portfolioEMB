b91ee2cd26094adf3755acd1619fe5f9ab4f5445
unknown <alex.chinchi02@gmail.com>
2025-05-21 12:04:47 +0200

    traduciendo

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Magia Web Dinámica con Next.js
+            Comprometidos con el futuro
           </p>
 
           {/**
@@ -52,17 +52,17 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transformando Conceptos en Experiencias de Usuario Perfectas"
+            words="Transformando conceptos en experiencias de usuario fluidas"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            ¡Hola! Somos el Equipo EMB, desarrolladores de Next.js.
+            Hola! Somos un grupo FullStack Developer
           </p>
 
           <a href="#about">
             <MagicButton
-              title="Mostrar nuestro trabajo"
+              title="Nuestros proyectos"
               icon={<FaLocationArrow />}
               position="right"
             />
